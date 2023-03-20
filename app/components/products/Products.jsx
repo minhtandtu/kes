472818a4086ca11ab726c8f479247bd07683dcb6
minhtandtu <minhtandtu@gmail.com>
2023-03-20 14:25:32 +0700

    secon

--- app/components/products/Products.jsx
+++ app/components/products/Products.jsx
@@ -1,13 +1,10 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import image from "../../../public/images/products.png";
 const Products = () => {
   return (
     <div className="">
-      <div className="container mx-auto py-20 px-16 bd">
-        <Image alt="Products" src={image}></Image>
-      </div>
+      <div className="container mx-auto py-20 px-16 bd"></div>
     </div>
   );
 };
